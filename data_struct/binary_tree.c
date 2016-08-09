92b1128b1d469967c38d75673d3e88d864af2084
Anish Shah <shahanish001@gmail.com>
2016-08-08 23:15:02 -0700

    Initial binary tree with test del left

--- data_struct/binary_tree.c
+++ data_struct/binary_tree.c
@@ -6,69 +6,57 @@ struct node {
 	int value;
 	struct node *left;
 	struct node *right;
+};
+
+struct node *find_node(struct node *head, int value)
+{
+	if(head == NULL) return NULL;
+
+	if(head->value == value)
+		return head;
+
+	if(value < head->value) {
+		return find_node(head->left, value);
+	} else if(value > head->value) {
+		return find_node(head->right, value);
+	}
 }
 
-/* Returns head of the tree after adding the vaue in the tree*/
+/* Returns pointer to the node pointing to the value */
 struct node* add_node(struct node *head, int value)
 {
 	struct node *temp = NULL;
 	struct node *curr = NULL;
 
+	if(head == NULL) return NULL;
+
 	temp = find_node(head, value);
-	if(temp) {
-		printf("Value %d exists\n", value);
-		return head;
-	}
+
+	if(temp) return temp;
 
 	temp = malloc(sizeof(struct node));
 	temp->left = NULL;
 	temp->right = NULL;
 	temp->value = value;
 
-	if(head == NULL){
-		return temp;
-	}
-
 	curr = head;
 
 	while(curr) {
-		if(value < curr->value && curr->left)
+		if(value < curr->value && curr->left) 
 			curr = curr->left;
 		else if(value > curr->value && curr->right)
 			curr = curr->right;
+		else 
+			break;
 	}
 
-	if(curr->value < value)
+	if(curr->value > value)
 		curr->left = temp;
 	else
 		curr->right = temp;
 
-	return head;
+	return temp;
 }
-
-
-struct node *find_node(struct node *head, int value)
-{
-	struct node* curr = NULL;
-
-	if(head == NULL)
-		return NULL;
-
-	curr = head;
-
-	while(curr) {
-		if(value < curr->value && curr->left)
-			curr = curr->left;
-		else if(value > curr->value && curr->right)
-			curr = curr->right;
-	}
-
-	if(curr->value == value)
-		return curr;
-	else
-		return NULL;
-}
-
 
 
 /*   Input stream: 5 3 9 7 1 13 2 0 11 15
@@ -103,18 +91,13 @@ int get_height(struct node *head)
 		return height_left;
 }
 
-void print_tree(struct node *head)
-{	
-	struct node *curr = NULL;
-
-	if(head == NULL)
-		printf("Invalid tree\n");
-
-	curr = head
-	while(curr) {
-
-	}
-
+void print_inorder(struct node *head)
+{
+	if(head == NULL) return;
+	if(head->left) print_inorder(head->left);
+	printf("%d \t", head->value);
+	if(head->right) print_inorder(head->right);
+	return;
 }
 
 /*   Input stream: 5 3 9 7 1 4 13 2 0 11 15
@@ -151,12 +134,14 @@ struct node *del_node(struct node *head, int value)
 		} else if(value > curr->value && curr->right) {
 			prev = curr;
 			curr = curr->right;
+		} else {
+			break;
 		}
 	}
 
 	if(curr->value != value) {
 		printf("Node node found %d\n", value);
-		return NULL
+		return NULL;
 	}
 
 	if(curr->left && curr->right) {
@@ -173,6 +158,34 @@ struct node *del_node(struct node *head, int value)
 
 
 int main(int argc, char const *argv[])
-{
+{	
+	struct node *head = NULL;
+	struct node *temp = NULL;
+	head = malloc(sizeof(struct node));
+	head->left = NULL;
+	head->right = NULL;
+	head->value = 5;
+	add_node(head, 3);
+	add_node(head, 9);
+	add_node(head, 3);
+	add_node(head, 7);
+	add_node(head, 1);
+	add_node(head, 4);
+	add_node(head, 9);
+	add_node(head, 13);
+	add_node(head, 2);
+	add_node(head, 0);
+	add_node(head, 11);
+	add_node(head, 15);
+
+	temp = find_node(head, 6);
+	if(temp) printf("found %d\n", temp->value);
+	temp = find_node(head, 7);
+	if(temp) printf("found %d\n", temp->value);
+
+	print_inorder(head); printf("\n");
+
+	printf("height %d\n", get_height(head));
+
 	return 0;
 }
